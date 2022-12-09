f0147eadf0b17bf6e202ca4271657c58b0216fed
imjh1 <limjh2255@gmail.com>
2022-12-10 00:42:20 +0900

    note to json, json to note

--- lib/notes/notes.dart
+++ lib/notes/notes.dart
@@ -3,17 +3,21 @@ import 'package:flutter_sound/public/flutter_sound_player.dart';
 import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 
 class Note {
-  final String title, thumbnail;
-  final int time;
+  /*final */late String title, thumbnail;
+  /*final */late int time;
 
-  FlutterSoundPlayer player = FlutterSoundPlayer();
-  FlutterSoundRecorder recorder = FlutterSoundRecorder();
-  bool isPlayerInited;
-  bool isRecorderInited;
-  bool isPlaybackReady;
+  late FlutterSoundPlayer player = FlutterSoundPlayer();
+  late FlutterSoundRecorder recorder = FlutterSoundRecorder();
+  late bool isPlayerInited;
+  late bool isRecorderInited;
+  late bool isPlaybackReady;
   List<DateTime> startTime = []; // 녹음 시작 시간
   List<DateTime> endTime = []; // 녹음 종료 시간
-  int cnt; // 녹음 파일 번호
+  late int cnt; // 녹음 파일 번호
+
+  /* Json에서 class 변환할 때 DateTime 배열로 변환 어려워 임시로 사용 */
+  List<String> tmp1 = [];
+  List<String> tmp2 = [];
 
   Note({
     required this.title,
@@ -24,6 +28,28 @@ class Note {
     this.isPlaybackReady = false,
     this.cnt = 0,
   });
+
+  Note.fromJson(Map<String, dynamic> json)
+    : title = json['title'],
+      tmp1 = List<String>.from(json['startTime']),
+      tmp2 = List<String>.from(json['endTime']),
+      cnt = json['cnt'];
+      /* DateTime으로 변환시 오류
+      startTime = List<DateTime>.from(Datetime.parse(json['startTime'].toString()),
+      endTime = List<DateTime>.from(json['endTime']);
+      */
+
+  Map<String, dynamic> toJson() {
+    /* DateTime -> String */
+    List<dynamic> startTime = this.startTime.map((i) => i.toString()).toList();
+    List<dynamic> endTime = this.endTime.map((i) => i.toString()).toList();
+    return {
+      'title': title,
+      'startTime': startTime,
+      'endTime': endTime,
+      'cnt': cnt,
+    };
+  }
 }
 
 List<Note> notes = [
