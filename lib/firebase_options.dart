05c301d4cb2e54932d882761290b706164c4a3eb
kimgiraffe <kimgiraffe@naver.com>
2022-12-12 17:18:33 +0900

    update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-            'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -28,19 +25,16 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
+          'you can reconfigure this by running the FlutterFire CLI again.',
         );
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
-              'you can reconfigure this by running the FlutterFire CLI again.',
+          'you can reconfigure this by running the FlutterFire CLI again.',
         );
       default:
         throw UnsupportedError(
@@ -49,9 +43,20 @@ class DefaultFirebaseOptions {
     }
   }
 
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBfd8-coguafpQvbzIeQS6P0SfZHdcQK-k',
+    appId: '1:333792263271:web:d90714b7806bc32f6d68c2',
+    messagingSenderId: '333792263271',
+    projectId: 'cse4186-ff4e8',
+    authDomain: 'cse4186-ff4e8.firebaseapp.com',
+    databaseURL: 'https://cse4186-ff4e8-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'cse4186-ff4e8.appspot.com',
+    measurementId: 'G-98ZGJF7BHP',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyDvVOdXRB9kZOswY1r3HG0cKIj99SQER78',
-    appId: '1:333792263271:android:49293963d264092a6d68c2',
+    appId: '1:333792263271:android:879d7e8481b77b7a6d68c2',
     messagingSenderId: '333792263271',
     projectId: 'cse4186-ff4e8',
     databaseURL: 'https://cse4186-ff4e8-default-rtdb.asia-southeast1.firebasedatabase.app',
@@ -60,12 +65,23 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyBhAceAGNtXdt5MoMb59YEInbR62XriAeE',
-    appId: '1:333792263271:ios:6f0994dcc487122a6d68c2',
+    appId: '1:333792263271:ios:988507ac96fd749b6d68c2',
     messagingSenderId: '333792263271',
     projectId: 'cse4186-ff4e8',
     databaseURL: 'https://cse4186-ff4e8-default-rtdb.asia-southeast1.firebasedatabase.app',
     storageBucket: 'cse4186-ff4e8.appspot.com',
-    iosClientId: '333792263271-jbhiimt5fcks437bcf9fujv9acj2nqpi.apps.googleusercontent.com',
-    iosBundleId: 'com.example.noteApp',
+    iosClientId: '333792263271-g9h30r8hjpnrun2o0apgcsu583sea6s4.apps.googleusercontent.com',
+    iosBundleId: 'com.example.knot',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyBhAceAGNtXdt5MoMb59YEInbR62XriAeE',
+    appId: '1:333792263271:ios:988507ac96fd749b6d68c2',
+    messagingSenderId: '333792263271',
+    projectId: 'cse4186-ff4e8',
+    databaseURL: 'https://cse4186-ff4e8-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'cse4186-ff4e8.appspot.com',
+    iosClientId: '333792263271-g9h30r8hjpnrun2o0apgcsu583sea6s4.apps.googleusercontent.com',
+    iosBundleId: 'com.example.knot',
   );
 }
